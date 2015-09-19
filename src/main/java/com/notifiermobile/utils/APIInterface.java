7687b99db224d98ee59a43008efa670ee58cf896
phieudu241 <phieudu241@gmail.com>
2015-09-20 06:11:50 +0700

    Add donate URL

--- src/main/java/com/notifiermobile/utils/APIInterface.java
+++ src/main/java/com/notifiermobile/utils/APIInterface.java
@@ -1,14 +1,15 @@
 package com.notifiermobile.utils;
 
-/**
- * Created by Administrator on 8/26/15.
- */
 public class APIInterface {
     //private static static String API_URL = "http://notifiermobile.com/api/notifier";
-    public static final String API_URL = "http://localhost:8282/api/notifications";
+    public static final String DOMAIN_URL = "http://192.168.1.5:8282";
+    public static final String API_URL = DOMAIN_URL + "/api/notifications";
+    public static final String API_USER_URL = DOMAIN_URL + "/api/user";
     public static final String GET_URL = API_URL;
     public static final String ADD_URL = API_URL;
     public static final String UPDATE_URL = API_URL;
     public static final String DELETE_URL = API_URL;
     public static final String MARK_AS_READ_URL = API_URL + "/markAsRead";
+    public static final String AUTHENTICATION = API_USER_URL + "/login";
+    public static final String DONATION = DOMAIN_URL + "/donate";
 }
